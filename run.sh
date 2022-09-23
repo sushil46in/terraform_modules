a057f22cc6f054fff75b95a5158a417aa5422e34
sushil46in <58732294+sushil46in@users.noreply.github.com>
2022-09-23 18:16:21 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-git remote add origin https://sushil46in:{{ secrets.GITHUB_TOKEN }}@github.com/sushil46in/terraform_generator.git
+git clone https://{{ secrets.GITHUB_TOKEN }}@github.com/sushil46in/terraform_generator.git
 copy request_provider.txt terraform_generator/Input/
 git config --global user.email "sushil46.in@outlook.com"
 git config --global user.name "Sushil Saini"
 git add .
 git commit -m "Synced new input"
-git push
+git push https://{{ secrets.GITHUB_TOKEN }}@github.com/sushil46in/terraform_generator.git
