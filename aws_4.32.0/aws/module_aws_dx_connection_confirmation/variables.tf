fdb032d03616fbd24f83fa139d629aa37c3ad64c
gitName <a@b.c>
2022-09-26 19:59:09 +1000

    test

--- aws_4.32.0/aws/module_aws_dx_connection_confirmation/variables.tf
+++
@@ -1,9 +0,0 @@
-/*This terraform file has been generated programmatically using terraform-generator.*/
-/*All the commented lines, if any, are optional. Remove comment characters, if required, before using.*/
-/*Refer https://github.com/sushil46in/terraform-generator.git for more details.*/
-
-variable "dx_connection_confirmation_connection_id" {
-  description = "(Required) The value for connection_id"
-  type = string
-}
-
--- aws_4.32.0/module_aws_ebs_encryption_by_default/outputs.tf
+++
@@ -1,8 +0,0 @@
-/*This terraform file has been generated programmatically using terraform-generator.*/
-/*All the commented lines, if any, are optional. Remove comment characters, if required, before using.*/
-/*Refer https://github.com/sushil46in/terraform-generator.git for more details.*/
-
-output "ebs_encryption_by_default_id" {
-  value = aws_ebs_encryption_by_default.resname.id
-}
-
